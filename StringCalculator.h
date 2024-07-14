2183a12b483e3fb03653be8be46763b1de52b631
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-07-14 21:09:55 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,26 +1,114 @@
 #include <string.h>
 #include <stdlib.h>
-int add(const char* input)
+/******************************************************************************
+ 
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+ 
+*******************************************************************************/
+ 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+ 
+int add_arr(int *Input,int cnt)
 {
-    int sum = 0, i = 0, j = 0;
-    char String[10] = {0};
-    for(i = 0; i < strlen(input); i++)
+    int Total = 0;
+	for(int i = 0; i < cnt; i++)
     {
-        if((input[i] >= 48) && (input[i] <= 57))
+        if(Input[i] < 1000)
         {
-            String[j++] = input[i];
-        }
-        else if((input[i] == 45) && (input[i+1] >= 48) && (input[i+1] <= 57))
-        {
-            printf("Exception: negatives not allowed:%c%c\n", input[i], input[i+1]);
-            exit(1);
-        }
-        else
-        {
-            sum += atoi(String) > 1000 ? 0: atoi(String);
-            memset(String, 0, 10);
-            j = 0;
+          Total += Input[i];
         }
     }
-    return sum += atoi(String) > 1000 ? 0: atoi(String);
+ 
+    return Total;
 }
+ 
+void strtonum(const char* String, int *num_arr,int *cnt)
+{
+    static int num = 0;
+ 
+	if((String[*cnt] >= 48) && (String[*cnt] <= 57))
+    {
+       num = num * 10 + String[*cnt] - '0';
+	  if((String[(*cnt)+1] >= 48) && (String[(*cnt)+1] <= 57))
+       {
+           (*cnt)++;
+       }
+       else
+       {
+          num_arr[*cnt] = num;
+          num = 0;
+          (*cnt)++;
+       }
+    }
+    else
+    {
+        (*cnt)++;
+        num = 0;
+    }
+}
+ 
+int sum(const char *String)
+{
+    int cnt =0;
+    int Total = 0;
+    int Number[100] = {0};
+    while(String[cnt] != '\0')
+    {
+       strtonum(String, Number, &cnt);
+    }
+ 
+    Total = add_arr(Number,cnt);
+ 
+    return Total;
+}
+ 
+int CheckNegNumber(const char *String,int i)
+{
+      if((String[i] == '-') && (String[i+1] >= 48) && (String[i+1] <= 57))
+      {
+		  printf("Exception: negatives not allowed: %c%c\n", String[i], String[i+1]);
+          return 0;
+      }
+    return 1;
+}
+ 
+int CheckStrNeg(const char *String)
+{
+    int Result = 0,i = 0;
+    while(String[i] != '\0')
+    {
+      Result = CheckNegNumber(String,i);
+      if(Result == 0)
+      {
+          break;
+      }
+      i++;
+    }
+    return Result;
+}
+ 
+int CheckStrEmpty(const char* String)
+{
+    return strlen(String) == 0 ? 1 : 0;
+}
+ 
+int CheckNegAndEmpty(const char *String)
+{
+	return (CheckStrEmpty(String) ? 1 : (CheckStrNeg(String) ? 1 : 0));
+}
+ 
+int add(const char *String)
+{
+    if((String != NULL) ? CheckNegAndEmpty(String) : 0)
+    {
+		int Result = sum(String);
+        return Result > 0 ? Result : 0; 
+    }
+ 
+    return -1;
+}
+ 
